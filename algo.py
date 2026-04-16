88a8620cc49bdb51c94b6c627147d9512bb1f3f9
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2026-04-16 21:45:57 +0200

    Implement trading logic in Trader class and add data model classes

--- algo.py
+++ algo.py
@@ -1,8 +1,12 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
-import string
 
 class Trader:
+
+    POSITION_LIMIT = 80
+    FAIR_PRICE_OSMIUM = 10000
+    BUY_PRICE_OSMIUM = 9998
+    SELL_PRICE_OSMIUM = 10010
 
     def bid(self):
         return 15
@@ -17,30 +21,39 @@ class Trader:
         # Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 10  # Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price))
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            if product != "ASH_COATED_OSMIUM":
+                result[product] = []
+                continue
+
+            else:
+                order_depth: OrderDepth = state.order_depths[product]
+                position = state.position.get(product, 0)
+
+
+                # Buy Algorithm
+                if len(order_depth.sell_orders) != 0:
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    available_to_buy = -best_ask_amount
+                    max_can_buy = self.POSITION_LIMIT - position
+
+                    if int(best_ask) < self.BUY_PRICE_OSMIUM and max_can_buy > 0:
+                        buy_qty = min(max_can_buy, available_to_buy)
+                        print("BUY", str(buy_qty) + "x", best_ask)
+                        orders.append(Order(product, best_ask, buy_qty))
+
+                # Sell Algorithm
+                if len(order_depth.buy_orders) != 0:
+                    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    max_can_sell = self.POSITION_LIMIT + position
+
+                    if int(best_bid) > self.SELL_PRICE_OSMIUM and max_can_sell > 0:
+                        sell_qty = min(max_can_sell, best_bid_amount)
+                        print("SELL", str(sell_qty) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -sell_qty))
+                
+                result[product] = orders
     
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-            
-            result[product] = orders
-    
-        # String value holding Trader state data required. 
-        # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
-        
-        # Sample conversion request. Check more details below. 
-        conversions = 1
+        traderData = ""  # No state needed - we check position directly
+        conversions = 0
         return result, conversions, traderData
